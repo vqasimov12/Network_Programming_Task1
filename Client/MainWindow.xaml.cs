f05515e018e5f0ef0eb3eaa0d662ab88107acafb
vqasimov12 <q.vaqif1211@gmail.com>
2024-09-12 22:16:07 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+
+namespace Client;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (file.Text.Length < 1) return;
+        using var client = new TcpClient();
+
+        var port = 27001;
+        var ip = IPAddress.Parse("192.168.1.8");
+        var ep = new IPEndPoint(ip, port);
+
+        try
+        {
+            client.Connect(ep);
+            if (client.Connected)
+            {
+                using (var source = new FileStream(file.Text, FileMode.Open, FileAccess.Read))
+                {
+                    using var bw = new BinaryWriter(client.GetStream());
+                    int len = 22;
+                    var bytes = new byte[len];
+                    var fileSize = source.Length;
+                    bw.Write(fileSize);
+
+                    do
+                    {
+                        len = source.Read(bytes, 0, bytes.Length);
+                        bw.Write(bytes, 0, len);
+                        fileSize -= len;
+                    } while (len > 0);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void Filebtn_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog op = new OpenFileDialog();
+        op.Title = "Select a picture";
+        op.Filter = "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg";
+        if (op.ShowDialog() == true)
+        {
+            file.Text = op.FileName;
+        }
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Server;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        _ = Task.Run(() => ChangeImage());
+    }
+
+
+    void ChangeImage()
+    {
+        var port = 27001;
+        var ip = IPAddress.Parse("192.168.1.8");
+        var endPoint = new IPEndPoint(ip, port);
+        using var listener = new TcpListener(endPoint);
+        listener.Start();
+        while (true)
+        {
+            var client = listener.AcceptTcpClient();
+            var task = Task.Run(() =>
+            {
+                try
+                {
+                    using var ns = client.GetStream();
+                    using var source = new FileStream("Image.jpeg", FileMode.Create, FileAccess.Write);
+                    var bytes = new byte[22];
+
+                    do
+                    {
+
+                        ns.Read(bytes, 0, bytes.Length);
+                        source.Write(bytes, 0, bytes.Length);
+
+                    } while (bytes.Length > 0);
+                    source.Close();
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        img.Source = new BitmapImage(new Uri("Image.jpeg", UriKind.RelativeOrAbsolute));
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+
+        }
+
+    }
+}
